e5c0a049ccc81ce8d29a9b6d6ba48f5abcbcb995
Rita MADIO <rita.madio@alten.it>
2026-04-13 15:34:05 +0200

    fixed sideBar routes

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { TooltipModule } from 'primeng/tooltip';
 import { CommonModule } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,5 +12,17 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  private router = inject(Router);
+
+  //per trasformare  url del router in signal reattivo
+  readonly currentUrl = toSignal(this.router.events);
   isExpanded = false;
+
+  goTo(path: string) {
+    this.router.navigate([path]);
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url.includes(path);
+  }
 }
