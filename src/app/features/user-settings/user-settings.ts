8cdf35a3f9153b9123277957880f32273fcfdac0
Rita MADIO <rita.madio@alten.it>
2026-04-20 12:25:38 +0200

    added confirmationService and MessageService in userSettings component

--- src/app/features/user-settings/user-settings.ts
+++ src/app/features/user-settings/user-settings.ts
@@ -12,6 +12,9 @@ import { Button } from 'primeng/button';
 import { Select } from 'primeng/select';
 import { InputText } from 'primeng/inputtext';
 import { SkeletonModule } from 'primeng/skeleton';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-user-settings',
@@ -26,13 +29,18 @@ import { SkeletonModule } from 'primeng/skeleton';
     Select,
     InputText,
     SkeletonModule,
+    ConfirmDialogModule,
+    ToastModule,
   ],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './user-settings.html',
   styleUrl: './user-settings.css',
 })
 export class UserSettings implements OnInit {
   private userService = inject(USER_SERVICE_TOKEN);
   users = this.userService.allUsers;
+  private confirmationService = inject(ConfirmationService); // per delete user
+  private messageService = inject(MessageService);
 
   loading = signal(false);
 
@@ -46,6 +54,14 @@ export class UserSettings implements OnInit {
   // Proprietà tradotte per l'header del dialog nel template
   editTitle = $localize`:@@userSettings.dialog.editTitle:Edit User`;
   newTitle = $localize`:@@userSettings.dialog.newTitle:New User`;
+
+  // messaggi toast
+  private showSuccess(detail: string) {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail });
+  }
+  private showError(detail: string) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail });
+  }
 
   ngOnInit() {
     this.updateMenu();
@@ -134,13 +150,21 @@ export class UserSettings implements OnInit {
       if (this.selectedUser) {
         const updatedUser = { ...this.selectedUser, ...formData };
         this.userService.updateUser(updatedUser).subscribe({
-          next: () => this.closeDialog(),
-          error: (err) => alert(err.message),
+          next: () => {
+            this.showSuccess($localize`:@@userSettings.msg.updated:User updated successfully`);
+            this.closeDialog();
+          },
+
+          error: (err) => this.showError(err.message),
         });
       } else {
         this.userService.addUser(formData).subscribe({
-          next: () => this.closeDialog(),
-          error: (err) => alert(err.message),
+          next: () => {
+            this.showSuccess($localize`:@@userSettings.msg.added:User added successfully`);
+            this.closeDialog();
+          },
+
+          error: (err) => this.showError(err.message),
         });
       }
     }
@@ -153,17 +177,23 @@ export class UserSettings implements OnInit {
   }
 
   deleteUser(user: User) {
-    const confirmDelete = confirm(
-      $localize`:@@userSettings.confirmDelete:Are you sure you want to delete the user ${user.firstName} ${user.lastName}?`,
-    );
-
-    if (confirmDelete && user.id) {
-      this.userService.deleteUser(user.id).subscribe({
-        next: () => {
-          console.log('User successfully deleted');
-        },
-        error: (err) => alert(err.message),
-      });
-    }
+    this.confirmationService.confirm({
+      message: $localize`:@@userSettings.confirmDelete:Are you sure you want to delete the user ${user.firstName} ${user.lastName}?`,
+      header: $localize`:@@userSettings.confirmHeader:Delete Confirmation`,
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: $localize`:@@userSettings.yes:Yes`,
+      rejectLabel: $localize`:@@userSettings.no:No`,
+      acceptButtonStyleClass: 'p-button-danger',
+      accept: () => {
+        if (user.id) {
+          this.userService.deleteUser(user.id).subscribe({
+            next: () => {
+              this.showSuccess($localize`:@@userSettings.msg.deleted:User deleted successfully`);
+            },
+            error: (err) => this.showError(err.message),
+          });
+        }
+      },
+    });
   }
 }
