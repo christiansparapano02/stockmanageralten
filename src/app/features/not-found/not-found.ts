de6d0e0d567abe380586be93df123bdac46f390f
Rita MADIO <rita.madio@alten.it>
2026-04-20 15:17:14 +0200

    feat: notFound component

---
+++ src/app/features/not-found/not-found.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.css',
+})
+export class NotFound {}
