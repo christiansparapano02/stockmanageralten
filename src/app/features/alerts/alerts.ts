d070890993dc54dbe82a1475f34b8bf8e27e1594
N0TPit <napoletanomichele00@gmail.com>
2026-04-29 17:06:15 +0200

    aggiunta skeleton alla dashboard

--- src/app/features/alerts/alerts.ts
+++ src/app/features/alerts/alerts.ts
@@ -1,15 +1,16 @@
 import { Component, inject, signal, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
 import { Item } from '../../core/item/item.interface';
 import { ItemMockService } from '../../core/item/item-mock.service';
+import { StatusLabelPipe } from "../../shared/pipes/status.pipe.ts";
 
 @Component({
   selector: 'app-alerts',
   standalone: true,
-  imports: [TableModule, ButtonModule, DatePipe, SkeletonModule],
+  imports: [TableModule, ButtonModule, DatePipe, SkeletonModule, NgClass, StatusLabelPipe],
   templateUrl: './alerts.html',
   styleUrl: './alerts.css',
 })
@@ -30,6 +31,12 @@ export class Alerts implements OnInit {
         this.loading.set(false);
       },
     });
+  }
+
+  getStatusClass(item: Item): string {
+    if (item.quantity === 0) return 'badge-critical';
+    if (item.quantity < item.minQuantity) return 'badge-low';
+    return 'badge-ok';
   }
 
   onRestock(item: Item) {
