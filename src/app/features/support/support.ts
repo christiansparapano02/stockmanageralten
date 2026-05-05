116b156077e772d4361d7eed5c1d1cd8703d9168
N0TPit <napoletanomichele00@gmail.com>
2026-05-05 15:33:26 +0200

    aggiunta message service in support e forget password

--- src/app/features/support/support.ts
+++ src/app/features/support/support.ts
@@ -2,22 +2,26 @@ import { Location } from '@angular/common';
 import { Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { InputTextModule } from 'primeng/inputtext';
 import { ProgressSpinner } from 'primeng/progressspinner';
 import { SelectModule } from 'primeng/select';
 import { TextareaModule } from 'primeng/textarea';
+import { Toast } from "primeng/toast";
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [ProgressSpinner, FormsModule, InputTextModule, TextareaModule, SelectModule],
+  imports: [ProgressSpinner, FormsModule, InputTextModule, TextareaModule, SelectModule, Toast],
+  providers: [MessageService],
   templateUrl: './support.html',
   styleUrl: './support.css',
 })
 export class Support implements OnInit {
   @ViewChild('form') form?: ElementRef<HTMLFormElement>;
-  logged = signal(true);
-
+  private messageService = inject(MessageService);
+  
+  logged = true;
   location = inject(Location);
 
   loading = signal(false);
@@ -47,19 +51,23 @@ selectedCategory = '';
   }
 
   onClick() {
-    if (this.logged()) {
-      window.alert(
-        'Il ticket di supporto è stato inviato\nStai venendo reindirizzato alla schermata di login...',
-      );
-      setTimeout(() => {
-        this.router.navigate(['/login']);
-      }, 1000);
-    } else {
-      window.alert(
-        'Il ticket di supporto è stato inviato\nStai venendo reindirizzato alla schermata principale...',
-      );
+    if (this.logged) {
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Il ticket di supporto è stato inviato!',
+        detail: 'Stai venendo reindirizzato alla schermata principale...'
+      });
       setTimeout(() => {
         this.router.navigate(['/categories']);
+      }, 1500);
+    } else {
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Il ticket di supporto è stato inviato!',
+        detail: 'Stai venendo reindirizzato alla schermata di login...',
+      });
+      setTimeout(() => {
+        this.router.navigate(['/login']);
       }, 1500);
     }
   }
